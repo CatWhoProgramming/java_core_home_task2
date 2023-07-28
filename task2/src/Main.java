ed4eb6c759e3a25f948cec411a3b2f200e3035cd
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 07:30:52 +0200

    Wrote method that called method areaOfCircle

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -1,36 +1,46 @@
 import task2.Car;
+import task5.MyArea;
 
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 2 ----------------------------");
         callCarClassFromTask2();
+        System.out.println("Task 3 ----------------------------");
         callCarClassFromTask3();
+        System.out.println("Task 4 ----------------------------");
         callCarClassFromTask4();
+        System.out.println("Task 5 ----------------------------");
+        calculateAreaOfCircle();
+    }
+
+    private static void calculateAreaOfCircle() {
+        System.out.println("area of circle " + MyArea.areaOfCircle(34.6));
     }
 
     private static void callCarClassFromTask4() {
         task4.Car car = new task4.Car();
-        System.out.println("Without parameters: " + car.getYear() + "\n" +
+        System.out.println("Without parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task4.Car(1876);
-        System.out.println("With one parameter: " + car.getYear() + "\n" +
+        System.out.println("With one parameter: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task4.Car(1987, 23.4);
-        System.out.println("With two parameters: " + car.getYear() + "\n" +
+        System.out.println("With two parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task4.Car(1234, 76.4, 2);
-        System.out.println("With three parameters: " + car.getYear() + "\n" +
+        System.out.println("With three parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task4.Car(1234, 76.4, 2, "blue");
-        System.out.println("With four parameters: " + car.getYear() + "\n" +
+        System.out.println("With four parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
@@ -40,27 +50,27 @@ public class Main {
 
     private static void callCarClassFromTask3() {
         task3.Car car = new task3.Car();
-        System.out.println("Without parameters: " + car.getYear() + "\n" +
+        System.out.println("Without parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task3.Car(1876);
-        System.out.println("With one parameter: " + car.getYear() + "\n" +
+        System.out.println("With one parameter: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task3.Car(1987, 23.4);
-        System.out.println("With two parameters: " + car.getYear() + "\n" +
+        System.out.println("With two parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task3.Car(1234, 76.4, 2);
-        System.out.println("With three parameters: " + car.getYear() + "\n" +
+        System.out.println("With three parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
         car = new task3.Car(1234, 76.4, 2, "blue");
-        System.out.println("With four parameters: " + car.getYear() + "\n" +
+        System.out.println("With four parameters: " + "\n" + car.getYear() + "\n" +
                 car.getSpeed() + "\n" +
                 car.getWeight() + "\n" +
                 car.getColor());
