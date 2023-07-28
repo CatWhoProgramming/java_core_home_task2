0fc7c11485b4d4b3c02bb91db6fe3dbe82e18517
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 07:29:40 +0200

    Wrote class MyArea with const

---
+++ task2/src/task5/MyArea.java
@@ -0,0 +1,9 @@
+package task5;
+
+public class MyArea {
+    private static final double PI = 3.14;
+
+    public static double areaOfCircle(double radius) {
+        return 2 * PI * (radius * radius);
+    }
+}
