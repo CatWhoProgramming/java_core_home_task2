81226a08a76140c511d61e3f1e070c954506776d
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 07:06:01 +0200

    wrote method for colling class Car from task3

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -1,17 +1,48 @@
 import task2.Car;
 
+
 public class Main {
     public static void main(String[] args) {
-        callCarClass();
-
+        callCarClassFromTask2();
+        callCarClassFromTask3();
     }
 
-    private static void callCarClass() {
+    private static void callCarClassFromTask3() {
+        task3.Car car = new task3.Car();
+        System.out.println("Without parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task3.Car(1876);
+        System.out.println("With one parameter: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task3.Car(1987, 23.4);
+        System.out.println("With two parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task3.Car(1234, 76.4, 2);
+        System.out.println("With three parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task3.Car(1234, 76.4, 2, "blue");
+        System.out.println("With four parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        System.out.println("----------------------------------------------------------");
+    }
+
+    private static void callCarClassFromTask2() {
         Car car = new Car();
-        System.out.println(car.getColor() + " "+ car.getYear());
+        System.out.println(car.getColor() + " " + car.getYear());
         car = new Car(3456, "green");
-        System.out.println(car.getColor() + " "+ car.getYear());
+        System.out.println(car.getColor() + " " + car.getYear());
         car = new Car("violet");
-        System.out.println(car.getColor() + " "+ car.getYear());
+        System.out.println(car.getColor() + " " + car.getYear());
+        System.out.println("----------------------------------------------------------");
     }
 }
