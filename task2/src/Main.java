7f885373b1f8adff22b83fd2b9cb95f31a8f27b3
CatWhoProgramming <kat.servetnik@gmail.com>
2023-07-28 07:19:33 +0200

    Wrote method for calling class Car from Task4

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -5,6 +5,37 @@ public class Main {
     public static void main(String[] args) {
         callCarClassFromTask2();
         callCarClassFromTask3();
+        callCarClassFromTask4();
+    }
+
+    private static void callCarClassFromTask4() {
+        task4.Car car = new task4.Car();
+        System.out.println("Without parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task4.Car(1876);
+        System.out.println("With one parameter: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task4.Car(1987, 23.4);
+        System.out.println("With two parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task4.Car(1234, 76.4, 2);
+        System.out.println("With three parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        car = new task4.Car(1234, 76.4, 2, "blue");
+        System.out.println("With four parameters: " + car.getYear() + "\n" +
+                car.getSpeed() + "\n" +
+                car.getWeight() + "\n" +
+                car.getColor());
+        System.out.println("----------------------------------------------------------");
+
     }
 
     private static void callCarClassFromTask3() {
